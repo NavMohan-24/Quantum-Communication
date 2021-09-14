24becf4ec4586446701ed0e9f99a18c7b6a53a94
Navaneeth Mohan <60849305+NavMohan-24@users.noreply.github.com>
2021-09-14 13:15:16 +0530

    Update Super Dense Coding.py

--- Super Dense Coding.py
+++ Super Dense Coding.py
@@ -1,15 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
 from qiskit import*
 from qiskit.tools.visualization import*
-
-
-# In[2]:
-
 
 def create_bellpair(qc,a,b):
     qc.h(a)
@@ -35,36 +27,22 @@ def decode_message(qc,a,b):
     qc.h(a) 
 
 
-# In[3]:
-
-
 qc=QuantumCircuit(2)
 create_bellpair(qc,0,1)
 message="10"
 encode_message(qc,0,message)
 decode_message(qc,0,1)
 qc.measure_all()
-qc.draw(output='mpl')
-
-
-# In[4]:
+#qc.draw('mpl')
 
 
 backend=BasicAer.get_backend('qasm_simulator')
-
 measurement_result=execute(qc,backend,shots=1024).result()
 counts=measurement_result.get_counts(qc)
 plot_histogram(counts)
 
-
 # Here using a single qubit alice can share 2 bit classical data with bob.
 # In similar manner using n qubit we can share 2^n classical bit data.
-# There are also a schemes for using a triparitate GHZ entanglement channel for doing superdense coding [link](arXiv:quant-ph/0610001).
+# There are also a schemes for using a triparitate GHZ entanglement channel for doing superdense coding (arXiv:quant-ph/0610001).
 # 
 # But this scheme also shares 2 bits of classical info. Here also alice shares only one qubit with bob
-
-# In[ ]:
-
-
-
-
